8aae0640751cf3e0773cdf49d48176f36edc5901
Alexandru-S <suleaa@tcd.ie>
2016-10-18 20:45:34 +0100

    added cos and sin functionality

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <iomanip>
 #include <string>
 #include <stdio.h>
+#include <math.h>
 using namespace std;
 main()
 {
@@ -14,10 +15,15 @@ main()
 		printf("********************\n");
 		printf("       Menu         \n");
 		printf("allowed signs +,-,*,/\n");
+		printf("allowed signs c for cos,s for sin\n");
 		printf("allowed format a [sign] b\n");
 		printf("********************\n");
 		printf("please input the numbers you want to add\n");
 		std::cin>>a>>sign>> b;
+		if(!cin)
+		{
+		printf("please input something\n");		
+		}
 		switch(sign)
 		{
 			case'+':
@@ -34,6 +40,14 @@ main()
 				break;
 			case'*':
 				result=a-b;
+				printf("Result:%f \n",result);
+				break;
+			case'c':
+				result=a*cos(b);
+				printf("Result:%f \n",result);
+				break;
+			case's':
+				result=a*sin(b);
 				printf("Result:%f \n",result);
 				break;
 			//printf("Result:%f \n",result);
